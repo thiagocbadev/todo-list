52142748d6f556c461463fd5afffd84ae94a1f1c
Thiagocbadev <thiagocba2599@gmail.com>
2025-10-24 19:42:51 -0300

    primeiro commit

---
+++ src/components/Search.jsx
@@ -0,0 +1,13 @@
+const Search = ({ search, setSearch }) => {
+    return (
+        <div className="search">
+            <input
+                type="text"
+                value={search} onChange={(e) => setSearch(e.target.value)}
+                placeholder="Digite para pesquisar.." />
+
+        </div>
+    )
+}
+
+export default Search
---
+++ src/components/Todo.jsx
@@ -0,0 +1,18 @@
+//import React from 'react'
+
+const todo = ({ todo, removeTodo, completeTodo }) => {
+    return (
+        <div className='todo' style={{ textDecoration: todo.isCompleted ? "line-through" : "" }}>
+            <div className='content'>
+                <p>{todo.text}</p>
+                <p className='category'>({todo.category})</p>
+                <div>
+                    <button className="complete" onClick={() => completeTodo(todo.id)} >Completar</button>
+                    <button className="remove" onClick={() => removeTodo(todo.id)}>x</button>
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default todo
